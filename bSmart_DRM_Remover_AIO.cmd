75f7b7880411d392fd06b97e989648c28c33db29
Tecnologica-Mente <117488150+Tecnologica-Mente@users.noreply.github.com>
2022-11-06 16:50:10 +0100

    Add files via upload

---
+++ bSmart_DRM_Remover_AIO.cmd
@@ -0,0 +1,132 @@
+@setlocal DisableDelayedExpansion
+@echo off
+
+
+::========================================================================================
+::
+::   This script is a DRM Protection Removal and Downloader for PDF in bSmart Platform.
+::
+::   Homepage: https://github.com/Tecnologica-Mente
+::      Email: <not available>
+::
+::   *************************************************************************************
+::
+::   Special thanks to Enrico for support
+::
+::========================================================================================
+
+
+
+
+::========================================================================================================================================
+:MainMenu
+
+cls
+color 07
+title  bSmart DRM Remover AIO v1.0.0
+mode 100, 30
+set "bsdrmrtemp=%SystemRoot%\Temp\__BSDRMR"
+if exist "%bsdrmrtemp%\.*" rmdir /s /q "%bsdrmrtemp%\" %nul%
+
+echo:
+echo:
+echo:             Welcome to bSmart DRM Remover AIO v1.0.0
+echo:
+echo:       ____________________________________________________________________________________
+echo:
+echo:             Please select:
+echo:
+echo:             [1] To install/upgrade all the required dependencies
+echo:             [2] To create/set/update your bSmart Cookie file after copying it
+echo:                 into the Windows ClipBoard from "_bsw_session_v1_production"
+echo:             [3] To remove the DRM protection and download your PDF eBook
+echo:             ________________________________________________________________________
+echo:                                                                     
+echo:             [4] Read Me
+echo:             [5] Exit
+echo:       ____________________________________________________________________________________
+echo:
+echo:             Enter a menu option in the Keyboard [1,2,3,4,5]:
+echo:
+choice /C:12345 /N
+set _erl=%errorlevel%
+
+if %_erl%==5 exit /b
+if %_erl%==4 start https://github.com/Tecnologica-Mente/bSmart_DRM_Remover & goto :MainMenu
+if %_erl%==3 setlocal & call :DownloadPDF       & cls & endlocal & goto :MainMenu
+if %_erl%==2 setlocal & call :SaveCookieFile    & cls & endlocal & goto :MainMenu
+if %_erl%==1 setlocal & call :IUDependencies    & cls & endlocal & goto :MainMenu
+goto :MainMenu
+
+::========================================================================================================================================
+:IUDependencies
+@setlocal DisableDelayedExpansion
+@echo off
+
+set mypath=%cd%
+::@echo %mypath%
+
+if not exist "package.json" (
+   echo Cannot find the package.json file. Operation aborted
+   goto :End
+)
+if not exist "%mypath%\node_modules\npm\bin\npm-cli.js" (
+   echo All the required dependencies has been already installed/updated
+) else (
+if exist "npm.cmd" (
+   call npm.cmd i
+   echo All the required dependencies has been installed/updated
+) else (
+   echo Cannot find the npm.cmd file. Make sure Node.js has been installed correctly
+)
+)
+:End
+echo:
+echo Press any key to continue...
+pause >nul
+popd
+exit /b
+
+::========================================================================================================================================
+:SaveCookieFile
+@setlocal DisableDelayedExpansion
+@echo off
+
+:: Adapted from: https://www.dostips.com/forum/viewtopic.php?t=3058
+set "path=bSmartCookieFile.txt"
+
+>"%temp%\clipboard.vbs" (
+   echo.Set objHTML = CreateObject("htmlfile"^)
+   echo.ClipboardText = objHTML.ParentWindow.ClipboardData.GetData("text"^)
+   echo.path = "%path%"
+   echo.Set objFSO = CreateObject("Scripting.FileSystemObject"^)
+   echo.Set objFile = objFSO.OpenTextFile(path, 2, true^)
+   echo.objFile.WriteLine ClipboardText
+   echo.objFile.Close )>>"%temp%\clipboard.vbs"
+"%temp%\clipboard.vbs"
+del /F /Q %temp%\clipboard.vbs
+echo bSmart Cookie file created successfully from Windows ClipBoard
+echo:
+echo Press any key to continue...
+pause >nul
+popd
+exit /b
+
+::========================================================================================================================================
+:DownloadPDF
+@setlocal DisableDelayedExpansion
+@echo off
+
+:: Adapted from: https://stackhowto.com/batch-file-to-check-if-multiple-files-exist/
+set mypath=%cd%
+::@echo %mypath%
+if exist "node.exe" if exist "download.js" call node download.js
+if not exist "node.exe" echo Cannot find the node.exe file. Operation aborted
+if not exist "download.js" echo Cannot find the download.js file. Operation aborted
+echo:
+echo Press any key to continue...
+pause >nul
+popd
+exit /b
+
+::========================================================================================================================================
---
+++ download.js
@@ -0,0 +1,147 @@
+const prompt = require('prompt-sync')({sigint: true});
+const fetch = require('node-fetch');
+const msgpack = require('msgpack-lite');
+const aesjs = require('aes-js');
+const PDFDocument = require('pdf-lib').PDFDocument;
+const fs = require('fs');
+const sanitize = require("sanitize-filename");
+
+let key = new Uint8Array([30, 0, 184, 152, 115, 19, 157, 33, 4, 237, 80, 26, 139, 248, 104, 155]);
+
+async function downloadAndDecryptFile(url) { 
+    
+    return new Promise(async (resolve, reject) => {
+        let file = await fetch(url, {method: "GET"}).then(res => res.buffer());
+
+        try {
+            let header = msgpack.decode(file.slice(0, 256));
+            console.log(header);
+
+            let firstPart = file.slice(256, header.start);
+            let secondPart = new Uint8Array(file.slice(header.start));
+
+            var aesCbc = new aesjs.ModeOfOperation.cbc(key, firstPart.slice(0, 16));
+            var decryptedFirstPart = aesCbc.decrypt(firstPart.slice(16));
+
+            for(let i=16;i>0;i--){
+                if (decryptedFirstPart.slice(decryptedFirstPart.length-i).every(e=>e==i)) {
+                    decryptedFirstPart = decryptedFirstPart.slice(0, decryptedFirstPart.length-i);
+                    break;
+                }
+            }
+
+            let result = new Uint8Array(decryptedFirstPart.length + secondPart.length);
+            result.set(decryptedFirstPart);
+            result.set(secondPart, decryptedFirstPart.length);
+            resolve(result);
+        } catch (e) {
+            reject({e, file})
+        }
+        
+    });
+    
+
+}
+
+// Adapted from: https://linuxhint.com/read-local-text-file-javascript/
+function showSelectedFile(){
+   fs.readFile("bSmartCookieFile.txt", (err, data) => {
+      if (err) throw err;
+
+      console.log(data.slice(0, -1).toString());  // slice(0, -1) remove the last character of a string but does not modify the original string (data)
+   });
+}
+
+(async () => {
+
+    let user = await fetch("https://www.bsmart.it/api/v5/user", {headers: {cookie:'_bsw_session_v1_production='+prompt('Input "_bsw_session_v1_production" cookie:')}});
+    //let user = await fetch("https://www.bsmart.it/api/v5/user", {headers: {cookie:'_bsw_session_v1_production='+showSelectedFile()}});
+
+    if (user.status != 200) {
+        console.log("Bad cookie");
+        return;
+    }
+
+    user = await user.json();
+
+    let headers = {"auth_token": user.auth_token};
+
+    let books = await fetch(`https://www.bsmart.it/api/v6/books?page_thumb_size=medium&per_page=25000`, {headers}).then(res => res.json());
+
+    if (books.length == 0) {
+        console.log('No books in your library!');
+    } else {
+        console.log("Book list:");
+        let i=0;
+        books.forEach((b) => {
+            console.log(`${i++} ${b.title}`);
+         });
+        
+    }
+    let bookId = prompt(`Please input book id${(books.length == 0 ? " manually" : "")}:`);
+
+    try {
+        let book = await fetch(`https://www.bsmart.it/api/v6/books/by_book_id/${books[bookId].id}`, {headers});
+
+        if (book.status != 200) {
+            console.log("Invalid book id");
+            return;
+        }
+
+        book = await book.json();
+
+        let info = [];
+        let page = 1;
+        while (true) {
+            //console.log(page);
+            let tempInfo = await fetch(`https://api.bsmart.it/api/v5/books/${book.id}/${book.current_edition.revision}/resources?per_page=500&page=${page}`, {headers}).then(res => res.json());
+            info = info.concat(tempInfo);
+            if (tempInfo.length < 500) break;
+            page++;
+        }
+    
+        console.log("Downloading pages");
+
+        const outputPdf = await PDFDocument.create()
+
+        for (i = 0; i<info.length; i++) {
+            for (j = 0; j<info[i].assets.length; j++) {
+
+                console.log(`Progress ${(i/info.length*100).toFixed(2)}%`);
+
+                if (info[i].assets[j].use != "page_pdf") continue;
+
+                let pageData = await downloadAndDecryptFile(info[i].assets[j].url).catch((e) => {console.log("Error Downloading page", e, i, j, info[i].assets[j].url)});
+
+                // if (md5(pageData) != info[i].assets[j].url) console.log("Missmatching md5 hash", i, j, info[i].assets[j].url)
+
+                //fs.writeFile(`temp/${i}-${j}.pdf`, pageData, (e)=>{});
+
+                const page = await PDFDocument.load(pageData);
+                const [firstDonorPage] = await outputPdf.copyPages(page, [0]);
+                outputPdf.addPage(firstDonorPage);
+
+            }
+        }
+
+        //fs.writeFile(prompt("Input file name:") + ".pdf", await outputPdf.save(), (e)=>{});
+
+        fs.writeFile(sanitize(book.id + " - " + book.title + ".pdf"), await outputPdf.save(), (e)=>{});
+
+        console.log("Saving . . .");
+
+    } catch(exception) {
+        if((bookId>books.length-1) || (bookId<0) || isNaN(bookId)){
+            console.log("Invalid book id");
+        } else {
+            //console.log(exception);
+            console.log("Ops! Something went wrong");
+        }
+        return;
+    }
+
+})();
+
+/*(async ()=> {
+    fs.writeFile("test.pdf", await downloadAndDecryptFile("https://s3-eu-west-1.amazonaws.com/dea.bsmart.it/0/9/092662be7c3701b1c596057205fc8a7e"), (e)=>{});
+})()*/
